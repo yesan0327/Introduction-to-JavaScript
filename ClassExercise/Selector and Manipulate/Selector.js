031f5cd00fe65c6230474ddbfb5207fe142d71fa
yesan0327 <353575688@qq.com>
2017-12-01 12:08:57 +0800

    selector

---
+++ ClassExercise/Selector and Manipulate/Selector.js
@@ -0,0 +1,26 @@
+// change the h1 color
+var h1=document.querySelector("h1");
+h1.style.color="yellow";
+
+//chang the background
+var body=document.querySelector("body");
+var isBlue=false;
+
+setInterval(function () { //manipulate
+    if(isBlue){
+        body.style.background="white";
+    }
+    else {
+        body.style.background="#3498db";
+    }
+    isBlue=!isBlue;
+},1000);
+
+//Exercise
+document.URL
+document.head
+document.body
+document.links
+
+//Methods
+document.getElementById()
---
+++ Homework/Function Problem Set/func.js
@@ -0,0 +1,17 @@
+function isEven (x) {
+    // if(x%2==0){
+    //    return console.log(true);
+    // }
+    // else
+    // return console.log(false);
+    return x%2===0;
+}
+
+function factorial(x) {
+    var product=1;
+        for(var i=2;i<=x;i++){
+            product=product*i;
+            return product;
+        }
+
+}
